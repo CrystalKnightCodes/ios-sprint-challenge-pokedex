74e8795db9ec74d794b49557e9c1c37476df582a
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-10 20:26:32 -0500

    Initial setup of poke search view controller

--- Pokedex/Pokedex/ApiController.swift
+++ Pokedex/Pokedex/ApiController.swift
@@ -7,3 +7,81 @@
 //
 
 import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+class APIController {
+    
+    private let baseUrl = URL(string: "https://pokeapi.co/api/v2")!
+    
+    // create function for fetching a pokemon
+    
+    func fetchPokemon(pokemonName: String, completion: @escaping (Result<Pokemon, NetworkError>) -> Void) {
+         // Need to add search name?
+        let pokemonURL = baseUrl.appendingPathComponent("pokemon/\(pokemonName)")
+        
+        
+        var request = URLRequest(url: pokemonURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                print("Error receiving pokemon name data: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let pokemonName = try decoder.decode(Pokemon.self, from: data)
+                completion(.success(pokemonName))
+                
+            } catch {
+                print("Error decoding pokemon objects: \(error)")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+        
+    }
+    
+    // create function to fetch image
+    func fetchImage(at urlString: String, completion: @escaping (Result<UIImage, NetworkError>) -> Void) {
+        let imageUrl = URL(string: urlString)!
+        
+        var request = URLRequest(url: imageUrl)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let _ = error {
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let image = UIImage(data: data)!
+            completion(.success(image))
+        }.resume()
+    }
+}
--- Pokedex/Pokedex/PokeSearchViewController.swift
+++ Pokedex/Pokedex/PokeSearchViewController.swift
@@ -9,22 +9,34 @@
 import UIKit
 
 class PokeSearchViewController: UIViewController {
-
+    // MARK: - Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func saveTapper(_ sender: UIButton) {
     }
-    */
+    
+}
 
+extension PokeSearchViewController: UISearchBarDelegate {
+        
 }
